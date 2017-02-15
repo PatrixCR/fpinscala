8a5b595f177723997c65ee14803661935b04f5bf
Patrix Rembang <patrixcr@gmail.com>
2017-02-15 22:54:15 +0700

    implement exercise 3.9

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -94,7 +94,7 @@ object List { // `List` companion object. Contains functions for creating and wo
     loop(l)
   }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int = foldRight(l, 0)((_, count) => count + 1)
 
   def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
 
