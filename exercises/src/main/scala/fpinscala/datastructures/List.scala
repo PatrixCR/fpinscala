01d54b0a586605c5d616d068474a36b56fd85e17
Patrix Rembang <patrixcr@gmail.com>
2017-04-08 23:45:53 +0700

    implement exercise 3.21

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -107,4 +107,6 @@ object List { // `List` companion object. Contains functions for creating and wo
   def filter[A](as: List[A])(f: A => Boolean): List[A] = foldRight(as, Nil:List[A])((a, b) => if (f(a)) Cons(a, b) else b)
 
   def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] = concat(map(as)(a => f(a)))
+
+  def filter2[A](as: List[A])(f: A => Boolean): List[A] = flatMap(as)(a => if (f(a)) Cons(a, Nil) else Nil)
 }
