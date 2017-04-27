27b70a8f1fec157789410bcf4fa589ad4d4ffaa8
Patrix Rembang <patrixcr@gmail.com>
2017-04-27 23:44:59 +0700

    fix exercise 4.4 & 4.5

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -52,14 +52,12 @@ object Option {
     a.foldRight(
       Some(List.empty[A]): Option[List[A]])(
       Option.map2(_, _)(_ :: _))
-      .filter(!_.isEmpty)
 
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
     a.foldRight(
       Some(List.empty[B]): Option[List[B]])(
       (aa, obs) =>
         Option.map2(f(aa), obs)(_ :: _))
-      .filter(!_.isEmpty)
 
   def sequence2[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(identity(_))
 
