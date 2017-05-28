c96164fa3f2edeb1c4a1ff7a3e07a38abe525e39
Patrix Rembang <patrixcr@gmail.com>
2017-05-28 22:28:15 +0700

    implement exercise 6.9

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -97,6 +97,10 @@ object RNG {
     val mod = i % n
     if (i + (n-1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
   }
+
+  def mapUsingFlatMap[A,B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(a => unit(f(a)))
+
+  def map2UsingFlatMap[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = flatMap(ra)(a => flatMap(rb)(b => unit(f(a, b))))
 }
 
 case class State[S,+A](run: S => (A, S)) {
